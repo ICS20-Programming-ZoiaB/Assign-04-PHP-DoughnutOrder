0c99805797e6045675da17b8777b9c4592e184b5
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-04-26 15:04:21 +0000

    re-added index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
    <!-- Metadata -->
    <meta charset="utf-8">
-   <meta name="description" content="Doughnut Order, with PHP">
+   <meta name="description" content="Doughnut Order">
    <meta name="keywords" content="immaculata, ics2o">
    <meta name="author" content="ZoiaB">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,135 +22,142 @@
    <link rel="stylesheet" href="./css/style.css">
    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-light_blue.min.css" />
 
-   
    <!-- Title -->
-   <title>Doughnut Order, with PHP</title>
+   <title>Doughnut Order</title>
  </head>
  <body>
+   
+   <!-- Link to js file -->
+   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+   <script src="./js/script.js"></script>
 
    <!-- Header -->
-   <h1>Doughnut Order, with PHP</h1>
+   <?php echo "<h1>Doughnut Order</h1>"; ?>
 
    <!-- Image -->
    <img src="./images/sprinkle_doughnut.webp" alt="Doughnuts" width="400" length="400">
      
    <!-- Text for welcome message -->
    <br><br>
-   <p>Welcome to our doughnut ordering webpage! Below are different sizes and toppings so that you can customize your doughnut.</p>
+   <?php echo "<p>Welcome to our doughnut ordering webpage! Below are different sizes and toppings so that you can customize your doughnut.</p>"; ?>
 
    <!-- Table for placing the menu/prices and user order side-by-side -->
-   <h4>Our Menu and Prices:</h4>
+   <?php echo "<h4>Our Menu and Prices:</h4>"; ?>
 
-         <!-- Table listing sizes -->
-         <table>
-           <tr>
-             <th class="border">Size</th>
-             <th class="border">Price</th>
-           </tr>
-           <tr>
-             <td class="border">Small</td>
-             <td class="border">$2.00</td>
-           </tr>
-           <tr>
-             <td class="border">Medium</td>
-             <td class="border">$3.00</td>
-           </tr>
-           <tr>
-             <td class="border">Large</td>
-             <td class="border">$4.00</td>
-           </tr>
-           <tr>
-             <td class="border">Frozen drink</td>
-             <td class="border">$3.00 each</td>
-           </tr>
-         </table>
+     <!-- Table listing sizes -->
+     <table>
+       <tr>
+         <th class="border">Size</th>
+         <th class="border">Price</th>
+       </tr>
+       <tr>
+         <td class="border">Small</td>
+         <td class="border">$2.00</td>
+       </tr>
+       <tr>
+         <td class="border">Medium</td>
+         <td class="border">$3.00</td>
+       </tr>
+       <tr>
+         <td class="border">Large</td>
+         <td class="border">$4.00</td>
+       </tr>
+       <tr>
+         <td class="border">Frozen drink</td>
+         <td class="border">$3.00 each</td>
+       </tr>
+     </table>
 
-        <!-- Table listing toppings -->
-         <br><br>
-         <table>
-           <tr>
-             <th class="border">Topping</th>
-             <th class="border">Price</th>
-           </tr>
-           <tr>
-             <td class="border">Sprinkles</td>
-             <td class="border">$0.50</td>
-           </tr>
-           <tr>
-             <td class="border">Chocolate shavings</td>
-             <td class="border">$0.50</td>
-           </tr>
-           <tr>
-             <td class="border">Skor bits</td>
-             <td class="border">$0.50</td>
-           </tr>
-           <tr>
-             <td class="border">Caramel syrup</td>
-             <td class="border">$0.50</td>
-           </tr>
-         </table>
-
-        <!-- Form for user input -->
-        <form action="./results.php" method="post" target="results">
-
-             <!-- Multiple choice - picking a doughnut size -->
-             <h4>Choose size and toppings:</h4>
-             <label for="size">Choose a size for the doughnut</label>
-             <select id="size" name="size">
-               <option value="0">-- Size --</option>
-               <option value="small">Small</option>
-               <option value="medium">Medium</option>
-               <option value="large">Large</option>
-             </select><br><br>
-
-             <!-- Multiple choice - picking toppings -->
-             <label for="flavour">Choose toppings</label>
-             <select id="flavour" name="flavour">
-               <option value="0">-- Toppings --</option>
-               <option value="skittles">Skittles</option>
-               <option value="chocolate shavings">Chocolate shavings</option>
-               <option value="skor bits">Skor bits</option>
-               <option value="caramel syrup">Caramel syrup</option>
-             </select>
-          
-             <!-- MDL checkboxes for Drink -->
-             <h4>Choose Your Frozen drink:</h4>
-          
-             <!-- First checkbox - Oreo drink -->
-             <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-               <input type="checkbox" id="oreo" name="oreo" class="mdl-checkbox__input">
-               <span class="mdl-checkbox__label">Oreo</span>
-             </label><br><br>
-
-             <!-- Second checkbox - Mango Drink -->
-             <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-               <input type="checkbox" id="mango" name="mango" class="mdl-checkbox__input">
-               <span class="mdl-checkbox__label">Mango</span>
-             </label><br><br>
-
-             <!-- Third checkbox - Fudge -->
-             <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-               <input type="checkbox" id="fudge" name="fudge" class="mdl-checkbox__input">
-               <span class="mdl-checkbox__label">Fudge</span>
-             </label><br><br>
-
-             <!-- Fourth checkbox - Raspberry Drink -->
-             <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
-               <input type="checkbox" id="raspberry" name="raspberry" class="mdl-checkbox__input">
-               <span class="mdl-checkbox__label">Raspberry</span>
-             </label><br><br>
-
-             <!-- MDL number input with floating label for drink -->
-             <h4>Choose number of drinks</h4>
-             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-               <input class="mdl-textfield__input" type="number" id="drinks" step="1" value="0" min="0" max="6" name="drinks">
-               <label class="mdl-textfield__label" for="drinks">...</label>
-             </div><br>
-
-             <!-- Adding id to submit button for styling -->
-             <input type="submit" id="red" value="Submit Your Order" button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">
-
-          <!-- Iframe -->
-          <iframe id="results" name="results"></iframe>
+   <!-- Table listing toppings -->
+   <br><br>
+   <table>
+     <tr>
+       <th class="border">Topping</th>
+       <th class="border">Price</th>
+     </tr>
+     <tr>
+       <td class="border">Sprinkles</td>
+       <td class="border">$0.50</td>
+     </tr>
+     <tr>
+       <td class="border">Chocolate shavings</td>
+       <td class="border">$0.50</td>
+     </tr>
+     <tr>
+       <td class="border">Skor bits</td>
+       <td class="border">$0.50</td>
+     </tr>
+     <tr>
+       <td class="border">Caramel syrup</td>
+       <td class="border">$0.50</td>
+     </tr>
+   </table>
+   
+   <!-- Form for user input -->
+   <form action="javascript:OrderSent()">
+     
+     <!-- Multiple choice - picking a doughnut size and drink type -->
+     <?php echo "<h4>Choose size of doughnut:</h4>"; ?>
+     <label for="doughnutSize">Choose a size for the doughnut</label>
+     <select id="doughnutSize" name="doughnutSize">
+       <option value="0">-- Size --</option>
+       <option value="small">Small</option>
+       <option value="medium">Medium</option>
+       <option value="large">Large</option>
+     </select><br><br>
+     
+     <!-- MDL checkboxes for Drink -->
+     <?php echo "<h4>Choose Your Doughnut Toppings:</h4>"; ?>
+     
+       <!-- First checkbox - Sprinkles -->
+       <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+         <span class="mdl-checkbox__label">Sprinkles</span>
+         <input type="checkbox" id="sprinkles" name="sprinkles" class="mdl-checkbox__input"><br><br>
+       </label>
+  
+       <!--Second checkbox - Chocolate Shavings -->
+       <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+         <span class="mdl-checkbox__label">Chocolate shavings</span>
+         <input type="checkbox" id="chocolate-shavings" name="chocolate-shavings" class="mdl-checkbox__input"><br><br>
+       </label>
+       
+       <!-- Third checkbox - Skor bits -->
+       <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+         <span class="mdl-checkbox__label">Skor bits</span>
+         <input type="checkbox" id="skor-bits" name="skor-bits" class="mdl-checkbox__input"><br><br>
+       </label>
+       
+       <!-- Fourth checkbox - Caramel syrup -->
+       <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+         <span class="mdl-checkbox__label">Caramel syrup</span>
+         <input type="checkbox" id="caramel-syrup" name="caramel-syrup" class="mdl-checkbox__input"><br><br>
+       </label>
+     
+     <!-- Multiple choice - drink type -->
+     <?php echo "<h4>Choose type of drink:</h4>"; ?>
+     <label for="drink">Choose drink type</label>
+     <select id="drink" name="drink">
+       <option value="0">-- Flavour --</option>
+       <option value="mango">Mango</option>
+       <option value="oreo">Oreo</option>
+       <option value="fudge">Fudge</option>
+       <option value="raspberry">Raspberry</option>
+     </select>
+     
+     <!-- MDL number input with floating label for drink -->
+     <?php echo "<h4>Choose number of drinks</h4>"; ?>
+     <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+       <input class="mdl-textfield__input" type="number" id="drinks-number" step="1" value="0" min="0" max="6" name="drinks-number">
+       <label class="mdl-textfield__label" for="drinks-number">Pick a number of drinks:</label>
+     </div>
+     <br>
+     
+     <!-- Submit button styling -->
+     <input type="submit" id="submit" value="Submit Your Order" button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">
+   
+     <!--Iframe for results-->
+     <iframe id="results" name="results"></iframe>
+     <br>
+   </form>
  </body>
 </html>
--- results.php
+++ results.php
@@ -45,4 +45,59 @@ $total = $subtotal + $tax;
 
 //displaying the totals to the screen
     echo "Your subtotal is $" . number_format($subtotal, 2) . ".<br>The amount of HST added is $" . number_format($tax, 2) . ".<br>Your total is $" . number_format($total, 2) . ".";
-?>
+
+
+
+
+
+
+
+
+
+
+
+
+ //initializing variables
+ let baseDoughnutCost = 0;
+ let doughnutSize = 0;
+
+//declaring variables for base doughnut and drink
+ let size = document.getElementById("doughnutSize").value;
+  let numDrinks = parseInt(document.getElementById("drinks-number").value);
+
+ //if the doughnut size is small
+ if (size == "small") {
+   baseDoughnutCost = 2.00;
+ }
+
+ //if the doughnut size is medium
+ else if (size == "medium") {
+   baseDoughnutCost = 3.00;
+ }
+
+
+ //if the doughnut size is large
+ else {
+   baseDoughnutCost = 4.00;
+ }
+
+ //determining the number of checkboxes checked
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+let numToppings = checkboxes.length;
+
+ //determining the cost of the toppings using a variable and a constant for the price
+ const UNIT_PRICE_TOPPINGS = 0.50;
+ let costToppings = numToppings * UNIT_PRICE_TOPPINGS;
+
+ //constant and variables for drink
+ const UNIT_PRICE_DRINKS = 3.00;
+ let costDrinks = numDrinks * UNIT_PRICE_DRINKS;
+
+ //calculating the subtotal, tax and total
+ let subtotal = baseDoughnutCost + costToppings + costDrinks;
+ let tax = subtotal * HST;
+ let total = subtotal + tax;
+
+ //displaying the totals to the screen
+ document.getElementById("results").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
+}
