4c8c93e813ac981a211092c8a76b1e8a2fd1465f
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-04-27 20:48:47 +0000

    ACTUALLY made php work by fixing checkbox count and getting rid of for...each

--- results.php
+++ results.php
@@ -7,6 +7,8 @@
  $baseDoughnutCost = 0;
  $doughnutSize = 0;
  $costToppings = 0;
+ $numToppings = 0;
+ $costDrinks = 0;
 
 //declaring variables for base doughnut and drink
  $size = $_POST["doughnutSize"];
@@ -22,23 +24,26 @@
    $baseDoughnutCost = 3.00;
  }
 
+ //if the doughnut size is not selected
+ else if ($size == 0) {
+   $baseDoughnutCost = 0.00;
+ }
+
  //if the doughnut size is large
  else {
    $baseDoughnutCost = 4.00;
  }
 
  //determining the number of checkboxes checked
-  if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  $numToppings = $_POST["checkboxes"];
-}
+ if(!empty($_POST["checkboxes"])) {
+   $numToppings = count($_POST["checkboxes"]);
+ }
 
  //determining the cost of the toppings using a variable and a constant for the price
  define("UNIT_PRICE_TOPPINGS", 0.50);
 
 //Turn costToppings into $topping to make it compatible to multiply with float
-foreach ($numToppings as $topping) {
-    $costToppings += $topping * UNIT_PRICE_TOPPINGS;
-}
+ $costToppings = $numToppings * UNIT_PRICE_TOPPINGS;
 
  //constant and variables for drink
  define("UNIT_PRICE_DRINKS", 3.00);
@@ -51,8 +56,6 @@ foreach ($numToppings as $topping) {
  $total = $subtotal + $tax;
 
  //displaying the totals to the screen
-foreach ($numToppings as $topping) {
-echo "Your order is a " . $size . " doughnut. " . "For your doughnut, you chose " . $topping . " toppings. You also decided to add " . $numDrinks . " drink(s) to your order.".
+echo "Your order is a " . $size . " doughnut. " . "For your doughnut, you chose " . $numToppings . " topping(s). You also decided to add " . $numDrinks . " drink(s) to your order.".
 " Your subtotal is $" . number_format($subtotal, 2) . ". The amount of HST added is $" . number_format($tax, 2) . ". Your total is $" . number_format($total, 2) . ".";
-}
 ?>
